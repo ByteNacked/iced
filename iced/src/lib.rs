daa58c477d2b5cd1e677f392eff41dc91ed89504
Роман Масленников <m3angreen@gmail.com>
2020-02-05 22:00:08 +0300

    WIP5

--- iced/src/lib.rs
+++ iced/src/lib.rs
@@ -14,6 +14,7 @@
 
 
 use core::ops::Range;
+
 use core::mem::transmute;
 use core::mem::size_of;
 pub enum MemoryError {
@@ -23,7 +24,13 @@ pub enum MemoryError {
     CrcNotMatched,
 }
 
+enum MemoryScan {
+    MetaV(&'static Meta),
+    EndOfMemory,
+    CrcNotMatched,
+}
 
+pub const MAX_PAYLOAD_SIZE : usize = 0x100;
 
 #[derive(Copy, Clone)]
 pub struct List {
@@ -43,7 +50,7 @@ pub trait ListStorage {
     fn head(&self, list : &List) -> Result<&'static [u8],()>;
 
     /// Get list with out first element
-    fn tail(&self, list : &List) -> Option<List>;
+    fn tail(&self, list : &List) -> Result<Option<List>, ()>;
 
     /// Append to head of list
     fn cons(&mut self, list : &List, buf : &[u8]) -> Result<List, ()>;
@@ -63,6 +70,35 @@ union UMeta {
     bytes: [u8; 4 * size_of::<usize>()],
 }
 
+impl<T, const N: usize> DefaultStorage<T,N> {
+    
+    //TODO: Some kind of crc check
+    
+    fn read_meta(&self, ptr : usize) -> Result<&'static Meta,()> {
+        let raw_bytes = self.read(ptr, size_of::<Meta>())?;
+        let meta : &Meta = unsafe {
+            let ptr = raw_bytes.as_ptr();
+            // ATTENTION!
+            transmute(&*ptr)
+        };
+
+        Ok(meta)
+    }
+
+    fn read_payload(&self, ptr : usize, sz : usize) -> Result<&'static [u8], ()> {
+        self.read(ptr, sz)
+    }
+
+    fn read_meta_checked(&self, ptr : usize) -> Result<&'static Meta,()> {
+        self.read_meta(ptr)
+    }
+
+    fn mark_chunk_invalid(&mut self, ptr : usize, sz : usize) -> Result<(),()> {
+        self.fill(ptr, sz, 0x00)
+    }
+
+}
+
 impl<T, const N: usize>  ListStorage for DefaultStorage<T,N> {
 
     type List = List;
@@ -70,32 +106,50 @@ impl<T, const N: usize>  ListStorage for DefaultStorage<T,N> {
 
     /// Scan and populate list of list saved lists
     fn scan(&self, ll : &mut Self::ListArray) -> Result<(),()> {
+        // Scan from zero to memory capacity or end early if 0xFFFFFF
+        let mut iter = 0;
+        // Find 
+        while iter < self.capacity() {
+            // TODO: some error handling
+            let meta = match self.read_meta_checked(iter) {
+                Ok(meta) => {
+                    debug_assert!(ll[meta.tag].tag == meta.tag);
+                    ll[meta.tag].ptr = iter;
+                    meta
+                }
+                Err(e) => return Err(e),
+            };
+            iter += size_of::<Meta>() + meta.sz;
+        }
+
         todo!()
     }
 
     /// Get first elemt of list
     fn head(&self, list : &List) -> Result<&'static [u8],()> {
-        //self.read(list.ptr)
-        todo!()
+        let meta = self.read_meta(list.ptr)?;
+        self.read_payload(list.ptr + size_of::<Meta>(), meta.sz)
     }
 
     /// Get list without first element
-    fn tail(&self, list : &List) -> Option<List> {
-        let raw_bytes = self.read(list.ptr).unwrap();
-        let meta : &Meta = unsafe {
-            let ptr = raw_bytes.as_ptr();
-            // ATTENTION!
-            transmute(&*ptr)
-        };
-        
-        todo!()
+    fn tail(&self, list : &List) -> Result<Option<List>, ()> {
+        let meta = self.read_meta(list.ptr)?;
+
+        if let 0 = meta.next {
+            Ok(None)
+        } else {
+            Ok(Some(List {
+                tag : meta.tag,
+                ptr : meta.next,
+            }))
+        }
     }
 
     /// Append to head of list
     fn cons(&mut self, list : &List, buf : &[u8]) -> Result<List, ()> {
         let next = list.ptr;
         let sz = buf.len();
-        let crc = 0;
+        let crc = 0xFACEFEED;
         let meta : UMeta = UMeta { meta : Meta{ tag : list.tag, next, sz, crc} };
         let meta_bytes  = unsafe { meta.bytes };
         let ptr = self.write(&meta_bytes)?;
@@ -121,7 +175,7 @@ impl<T : Sized, const N: usize>  DefaultStorage<T,N> {
         }
     }
 
-    pub fn read(&self, ptr : usize) -> Result<&'static [u8],()> {
+    pub fn read(&self, ptr : usize, sz : usize) -> Result<&'static [u8],()> {
         todo!()
     }
 
@@ -129,12 +183,19 @@ impl<T : Sized, const N: usize>  DefaultStorage<T,N> {
         todo!()
     }
 
+    fn fill(&mut self, ptr : usize, sz : usize, b : u8) -> Result<(),()> {
+        todo!()
+    }
+
     pub fn len(&self) -> usize {
         self.sz
     }
+
+    pub fn capacity(&self) -> usize {
+        N
+    }
+
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -144,4 +205,5 @@ mod tests {
     fn array_test() {
         let array = DefaultStorage([0u8; 10]);
     }
+
 }
