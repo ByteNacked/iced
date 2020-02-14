3ffc1c9695d287dc3ac8203a75676b9bd97b4b9d
Roman M <m3angreen@gmail.com>
2020-02-14 05:46:20 +0300

    WIP8

--- iced/src/lib.rs
+++ iced/src/lib.rs
@@ -3,7 +3,9 @@
     const_fn,
     const_fn_union,
     const_generics,
+    const_if_match,
     const_mut_refs,
+    const_panic,
     const_transmute,
     maybe_uninit_extra,
     maybe_uninit_ref,
@@ -12,13 +14,22 @@
     untagged_unions
 )]
 
+#![allow(dead_code, unused_imports)]
+#![allow(incomplete_features)]
 
-use core::ops::Range;
-use core::mem::transmute;
 use core::mem::size_of;
-use core::slice::from_raw_parts_mut;
-use crc::crc32::{Digest, IEEE_TABLE, IEEE, Hasher32};
+use core::slice::{from_raw_parts_mut, from_raw_parts};
 
+// TODO: implement errors
+// TODO: WORD_SIZE const
+
+pub trait StorageHasher32 {
+    fn reset(&mut self);
+
+    fn write(&mut self, words: &[u32]);
+
+    fn sum(&self) -> u32;
+}
 
 pub enum Error {
     OutOfSpace,
@@ -27,304 +38,317 @@ pub enum Error {
     BadAlignment,
 }
 
-enum MemoryScan {
-    EndOfMemory,
-    CrcNotMatched,
-}
-
 pub struct InitStats {
-    err_cnt : usize,
+    words_wasted : usize,
     unique_tags : usize,
 }
 
-pub const MAX_PAYLOAD_SIZE : usize = 0x100;
-
 #[repr(C)]
+#[derive(PartialEq, Eq, Debug)]
 pub struct Header {
-    tag  : usize,
-    sz   : usize,
-    crc  : usize,
+    tag  : u32,
+    sz   : u32,
+    crc  : u32,
 }
 
-#[repr(C)]
-union UHeader {
-    header : Header,
-    bytes: [u8; size_of::<Header>() * size_of::<usize>()],
-}
-
-#[derive(Copy, Clone)]
-pub struct Record {
-    pub tag : usize,
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub struct RecordDesc {
+    pub tag : u32,
     pub ptr : Option<&'static Header>,
 }
 
-struct Storage {
-    align : usize,
-    ptr : usize,
-    capacity : usize,
-    size  : usize,
+pub struct Storage {
+    storage : &'static mut [u32],
+    current  : usize,
 }
 
 impl Storage {
-    pub const fn new(start_addr : usize, align : usize, capacity : usize) -> Self {
+
+    /// Length in words
+    const LEN_OF_HEADER : usize = size_of::<Header>() / size_of::<u32>();
+
+    pub fn new(start_addr : usize, capacity : usize) -> Self {
+        assert!(start_addr % 4 == 0);
+        assert!(capacity % 4 == 0);
+
+        let storage = unsafe { from_raw_parts_mut(start_addr as *mut _, capacity / size_of::<u32>()) };
+
         Self {
-            align,
-            ptr : start_addr,
-            capacity,
-            size : 0,
+            storage,
+            current : 0,
         }
     }
+    
+    /// Scan through storage memory and populate record descriptor table
+    pub fn init(&mut self, list : &mut [RecordDesc], hasher : &mut impl StorageHasher32) -> InitStats {
 
-    pub fn init(&mut self, list : &mut [Record], crc : &mut impl Hasher32) -> InitStats {
-
-        let mut stats = InitStats { err_cnt : 0, unique_tags : 0 };
+        let mut stats = InitStats { words_wasted : 0, unique_tags : 0 };
         
         let mut idx = 0;
         let mut size = 0;
-        while idx < self.capacity - size_of::<Header>() {
-            match self.header_validate(idx, crc) {
-                Ok(sz) => {
-                    let header = self.header_from_idx(idx);
-                    list[header.tag].ptr = Some(header);
-                    idx += sz;
-                    size += size;
+        let mut last_valid_end = 0;
+        let capacity = self.storage.len();
+        
+        // Scanning through whole storage to find all valid records
+        while idx < capacity - Self::LEN_OF_HEADER {
+            let res = self.validate_record(idx, hasher);
+            match res {
+                Ok(header) => {
+                    list[header.tag as usize].ptr = Some(header);
+                    idx += Self::LEN_OF_HEADER + header.sz as usize;
+                    last_valid_end = idx;
                 }
                 Err(_) => {
                     idx += 1;
                 }
             }
         }
-        self.size = size;
+        
+        // Scannig from last record end position, to determine that
+        // rest flash memory wasn't already written (NOT 0xFF'ed)
+        for idx in last_valid_end .. capacity {
+            if !Self::is_ffed(self.storage[idx]) {
+                size = idx + 1;
+                stats.words_wasted += 1;
+            }
+        }
+
+        self.current = size;
+
+        // Stats
+        for e in list {
+            if let Some(_) = &e.ptr {
+                stats.unique_tags += 1;
+            }
+        }
         
         stats
     }
+
+    fn validate_record(&self, idx : usize, hasher : &mut impl StorageHasher32) -> Result<&'static Header,()> {
+        let _tag = self.storage[idx];
+        let len = self.storage[idx + 1];
+        let crc = self.storage[idx + 2];
+
+        let payload_start_idx = idx + 3;
+        let payload_end_idx = payload_start_idx + len as usize;
+        // Check payload slice is not out of bounds
+        if payload_end_idx > self.storage.len() {
+            return Err(());
+        }
+        
+        // Calculate checksum
+        hasher.reset();
+        let header_part = &self.storage[idx .. idx + 2];
+        hasher.write(header_part);
+        let payload_slice = &self.storage[payload_start_idx .. payload_end_idx];
+        hasher.write(payload_slice);
+        
+        // Compare checksums
+        let calc_crc = hasher.sum();
+        if crc != calc_crc {
+            return Err(());
+        }
+        
+        let header : &Header = unsafe { &*(self.storage[idx .. ].as_ptr() as *const _) };
+        Ok(header)
+    }
     
-    pub fn update(&mut self, record : &mut Record, buf : &[u8], crc : &mut impl Hasher32) -> Result<(),()> {
+    // Update recordy entry
+    pub fn update(&mut self, record : &mut RecordDesc, payload : &[u32], hasher : &mut impl StorageHasher32) -> Result<(),()> {
+        let record_len = Self::LEN_OF_HEADER + payload.len();
+        if self.free_space_in_words() < record_len {
+            return Err(());
+        }
+
+        let record_slice = &mut self.storage[self.current .. self.current + record_len];
+        let (header_slice, payload_slice) = record_slice.split_at_mut(Self::LEN_OF_HEADER);
+
+        // Fill header
+        header_slice[0] = record.tag;
+        header_slice[1] = payload.len() as u32;
+
+        // Copy payload
+        payload_slice.copy_from_slice(payload);
+        
+        // Calculate and set checksum
+        hasher.reset();
+        hasher.write(&header_slice[0 .. 2]);
+        hasher.write(payload_slice);
+        let checksum = hasher.sum();
+        header_slice[2] = checksum;
+
+        // Update record descriptor
+        let updated_header : &Header = unsafe { &*(header_slice.as_ptr() as *const Header) };
+        record.ptr = Some(updated_header);
+
+        // Update current len
+        self.current += record_len;
+
+        Ok(())
+    }
+    
+    /// Get record payload
+    pub fn get(&mut self, record : &RecordDesc) -> Result<&'static [u32],()> {
+        match record.ptr {
+            Some(header) => {
+                // Basic sanity check
+                if header.tag == record.tag {
+                    unsafe {
+                        let header_ptr = header as *const _ as *const u32;
+                        let payload_ptr = header_ptr.offset(Self::LEN_OF_HEADER as isize);
+                        Ok(from_raw_parts(payload_ptr, header.sz as usize))
+                    }
+                } else {
+                    Err(())
+                }
+            },
+            None => Err(()),
+        }
+    }
+
+    /// Total amount of occupied storage space in bytes
+    pub fn len(&self) -> usize {
+        self.current * size_of::<u32>()
+    }
+    /// Total storage space in bytes
+    pub fn capacity(&self) -> usize {
+        self.storage.len() * size_of::<u32>()
+    }
+
+    fn free_space_in_words(&self) -> usize {
+        self.storage.len() - self.current
+    }
+
+    fn is_ffed(word : u32) -> bool {
+        if word == !0 {
+            return true;
+        }
+        return false;
+    }
+
+    fn header_from_slice(&self, _slice : &'static [u32]) -> &'static Header {
         todo!()
     }
 
-    pub fn get(&mut self, record : &Record) -> Result<&'static [u8],()> {
+
+    fn payload_from_header_slice(&self, _header : &'static Header) -> Result<&'static [u32],()> {
         todo!()
-    }
-
-    fn header_validate(&self, idx : usize, crc : &mut impl Hasher32) -> Result<usize,()> {
-        let header = self.header_from_idx(idx);
-        let payload_idx = idx + size_of::<Header>();
-        
-        // Check size in bounds
-        let payload_idx_end = payload_idx + header.sz;
-        if payload_idx_end > self.capacity {
-            return Err(());
-        }
-        let total_sz = size_of::<Header>() + header.sz;
-
-        // Calculate crc
-        crc.reset();
-        crc.write(self.slice_from_idx(idx, total_sz));
-        let crc = crc.sum32() as usize;
-        
-        if crc != header.crc {
-            return Err(());
-        }
-
-        Ok(total_sz)
-    }
-    
-    fn is_slice_ffed(&self, slice : &[u8]) -> bool {
-        for i in slice.iter() {
-            if *i != !0 {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    fn header_from_idx(&self, idx : usize) -> &'static Header {
-        debug_assert!(idx % self.align == 0);
-        debug_assert!(idx + size_of::<Header>() <= self.capacity);
-        unsafe { transmute(self.ptr + idx) }
-    }
-
-    fn slice_from_idx(&self, idx : usize, sz : usize) -> &mut [u8] {
-        debug_assert!(idx % self.align == 0);
-        debug_assert!(idx + sz <= self.capacity);
-        unsafe { from_raw_parts_mut( idx as *mut _ , sz) }
     }
 }
 
 
-
-
-///pub trait ListStorage {
-///
-///    type List;
-///    type ListArray;
-///
-///    /// Scan and populate list of list saved lists
-///    fn scan(&self, ll : &mut Self::ListArray) -> Result<(),()>;
-///
-///    /// Get first elemt of list
-///    fn head(&self, list : &List) -> Result<&'static [u8],()>;
-///
-///    /// Get list with out first element
-///    fn tail(&self, list : &List) -> Result<Option<List>, ()>;
-///
-///    /// Append to head of list
-///    fn cons(&mut self, list : &List, buf : &[u8]) -> Result<List, ()>;
-///}
-///
-///impl<T, const N: usize> DefaultStorage<T,N> {
-///    
-///    //TODO: Some kind of crc check
-///    
-///    fn read_meta(&self, ptr : usize) -> Result<&'static Meta,()> {
-///        let raw_bytes = self.read(ptr, size_of::<Meta>())?;
-///        let meta : &Meta = unsafe {
-///            let ptr = raw_bytes.as_ptr();
-///            // ATTENTION!
-///            transmute(&*ptr)
-///        };
-///
-///        Ok(meta)
-///    }
-///
-///    fn read_payload(&self, ptr : usize, sz : usize) -> Result<&'static [u8], ()> {
-///        self.read(ptr, sz)
-///    }
-///
-///    fn read_meta_checked(&self, ptr : usize) -> Result<&'static Meta,()> {
-///        self.read_meta(ptr)
-///    }
-///
-///    fn mark_chunk_invalid(&mut self, ptr : usize, sz : usize) -> Result<(),()> {
-///        self.fill(ptr, sz, 0x00)
-///    }
-///
-///}
-///
-///impl<T, const N: usize>  ListStorage for DefaultStorage<T,N> {
-///
-///    type List = List;
-///    type ListArray = [List; N];
-///
-///    /// Scan and populate list of list saved lists
-///    fn scan(&self, ll : &mut Self::ListArray) -> Result<(),()> {
-///        // Scan from zero to memory capacity or end early if 0xFFFFFF
-///        let mut iter = 0;
-///        // Find 
-///        while iter < self.capacity() {
-///            // TODO: some error handling
-///            let meta = match self.read_meta_checked(iter) {
-///                Ok(meta) => {
-///                    debug_assert!(ll[meta.tag].tag == meta.tag);
-///                    ll[meta.tag].ptr = iter;
-///                    meta
-///                }
-///                Err(e) => return Err(e),
-///            };
-///            iter += size_of::<Meta>() + meta.sz;
-///        }
-///
-///        todo!()
-///    }
-///
-///    /// Get first elemt of list
-///    fn head(&self, list : &List) -> Result<&'static [u8],()> {
-///        let meta = self.read_meta(list.ptr)?;
-///        self.read_payload(list.ptr + size_of::<Meta>(), meta.sz)
-///    }
-///
-///    /// Get list without first element
-///    fn tail(&self, list : &List) -> Result<Option<List>, ()> {
-///        let meta = self.read_meta(list.ptr)?;
-///
-///        if let 0 = meta.next {
-///            Ok(None)
-///        } else {
-///            Ok(Some(List {
-///                tag : meta.tag,
-///                ptr : meta.next,
-///            }))
-///        }
-///    }
-///
-///    /// Append to head of list
-///    fn cons(&mut self, list : &List, buf : &[u8]) -> Result<List, ()> {
-///        let next = list.ptr;
-///        let sz = buf.len();
-///        let crc = 0xFACEFEED;
-///        let meta : UMeta = UMeta { meta : Meta{ tag : list.tag, next, sz, crc} };
-///        let meta_bytes  = unsafe { meta.bytes };
-///        let ptr = self.write(&meta_bytes)?;
-///        let _   = self.write(&buf)?;
-///
-///        Ok(List {
-///            tag : list.tag,
-///            ptr,
-///        })
-///    }
-///}
-///
-///pub struct DefaultStorage<T, const N: usize> {
-///    buf : [T; N],
-///    sz : usize,
-///}
-///
-///impl<T : Sized, const N: usize>  DefaultStorage<T,N> {
-///    pub const fn new(array : [T;N]) -> Self {
-///        Self {
-///            buf : array,
-///            sz : 0,
-///        }
-///    }
-///
-///    pub fn read(&self, ptr : usize, sz : usize) -> Result<&'static [u8],()> {
-///        todo!()
-///    }
-///
-///    pub fn write(&mut self, buf : &[u8]) -> Result<usize, ()> {
-///        todo!()
-///    }
-///
-///    fn fill(&mut self, ptr : usize, sz : usize, b : u8) -> Result<(),()> {
-///        todo!()
-///    }
-///
-///    pub fn len(&self) -> usize {
-///        self.sz
-///    }
-///
-///    pub fn capacity(&self) -> usize {
-///        N
-///    }
-///
-///}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crc::crc32::{Digest, IEEE_TABLE, IEEE, Hasher32};
+    use crc::CalcType;
 
-    #[test]
-    fn array_test() {
-        //let array = DefaultStorage([0u8; 10]);
-        //let crc = Digest::new(crc32::IEEE);
+    impl StorageHasher32 for Digest {
+        fn reset(&mut self) {
+            <Digest as Hasher32>::reset(self);
+        }
+
+        fn write(&mut self, words: &[u32]) {
+            let bytes = unsafe { 
+                from_raw_parts(words.as_ptr() as *const u8, words.len() * size_of::<u32>()) 
+            };
+            <Digest as Hasher32>::write(self, bytes);
+        }
+
+        fn sum(&self) -> u32 {
+            <Digest as Hasher32>::sum32(self)
+        }
     }
 
+    fn crc32_ethernet() -> impl StorageHasher32 {
+        Digest::new_custom(IEEE, !0u32, 0u32, CalcType::Normal)
+    }
+
+    fn new_params_from_array(storage_mem : &mut [u32] ) -> (usize, usize) {
+        let start_addr = storage_mem.as_mut_ptr() as usize;
+        let capacity = size_of::<u32>() * storage_mem.len();
+
+        (start_addr, capacity)
+    }
+
+    fn new_storage(storage_mem : &mut [u32] ) -> Storage {
+        let start_addr = storage_mem.as_mut_ptr() as usize;
+        let capacity = size_of::<u32>() * storage_mem.len();
+
+        Storage::new(start_addr, capacity)
+    }
 
     #[test]
-    fn crc32_test() {
-        let mut crc = Digest::new(IEEE);
-        let b = [0xA5u8];
-        crc.write(&b);
-        let res : u32 = crc.sum32();
-        assert_eq!(res, 0x74BEB8EA);
+    fn empty_test() {
+        let mut storage_mem = [!0u32;0x100];
+        let (start_addr, capacity) = new_params_from_array(&mut storage_mem[..]);
+        let storage = Storage::new(start_addr, capacity);
+
+        assert_eq!(storage.len(), 0);
+        assert_eq!(storage.capacity(), capacity);
+    }
+    
+    #[test]
+    fn new_record_test() {
+        let mut storage_mem = [!0u32;0x100];
+        let mut storage = new_storage(&mut storage_mem[..]);
+        let mut rec_desc = RecordDesc {
+            tag : 1,
+            ptr : None,
+        };
+
+        let rec_payload = [42u32;1];
+        let mut crc32 = crc32_ethernet();
         
-        crc.reset();
-        let b = [0xA5,0xA5,0xA5,0xA5];
-        crc.write(&b);
-        let res : u32 = crc.sum32();
-        assert_eq!(res, 0xF18EB66B);
+        storage.update(&mut rec_desc, &rec_payload, &mut crc32).unwrap();
+        assert_eq!(storage.len(), (Storage::LEN_OF_HEADER + rec_payload.len()) * size_of::<u32>() );
+        assert!(&rec_desc.ptr.is_some());
+        
+        let out_rec_payload = storage.get(&rec_desc).unwrap();
+        assert_eq!(&rec_payload, out_rec_payload);
+
+
+        let mut desc_list = [
+            RecordDesc {
+                tag : 0,
+                ptr : None,
+            },
+            RecordDesc {
+                tag : 1,
+                ptr : None,
+            },
+        ];
+        let stats = storage.init(&mut desc_list, &mut crc32);
+        assert_eq!(&desc_list[1], &rec_desc);
+
+        //println!("Desc list : {:#?}", &desc_list);
     }
+
+    //#[test]
+    //fn crc32_test() {
+    //    let mut crc = Digest::new_custom(IEEE, !0u32, 0u32, CalcType::Normal);
+
+    //    crc.reset();
+    //    let b = [0xA5u8];
+    //    crc.write(&b);
+    //    let res : u32 = crc.sum32();
+    //    println!("\n{:x}\n", &res);
+    //    assert_eq!(res, 0xA8E282D1);
+
+    //    crc.reset();
+    //    let b = [0xA5u8, 0];
+    //    crc.write(&b);
+    //    let res : u32 = crc.sum32();
+    //    println!("\n{:x}\n", &res);
+    //    assert_eq!(res, 0xA8E282D1);
+    //    
+    //    crc.reset();
+    //    let b = [0xA5,0xA5,0xA5,0xA5];
+    //    crc.write(&b);
+    //    let res : u32 = crc.sum32();
+    //    assert_eq!(res, 0x29928E70);
+    //}
 
 }
 
